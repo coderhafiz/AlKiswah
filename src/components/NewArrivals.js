20ab08d13cae272574005145449fbc59ad687811
Coderhafiz <hafizabd457@gmail.com>
2025-12-28 17:46:00 +0100

    Al-kiswah update

--- src/components/NewArrivals.js
+++ src/components/NewArrivals.js
@@ -21,7 +21,7 @@ export default function NewArrivals() {
     <>
       <section className="py-12 bg-purple-50 " id="new-arrivals">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-12 md:mb-16">
+          <div className="text-center mb-4 md:mb-16">
             <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
               New Arrivals
             </h2>
@@ -29,7 +29,7 @@ export default function NewArrivals() {
               Fresh styles just for you.
             </p>
           </div>
-          <div className="overflow-hidden py-16 px-3" ref={emblaRef}>
+          <div className="overflow-hidden py-4 md:py-16 px-3" ref={emblaRef}>
             <div className="flex">
               {newArrivals.map((product) => (
                 <div
